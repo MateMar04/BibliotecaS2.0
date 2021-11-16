fae0380126965641cc6eff4849affa083fe7ce02
MateMar04 <mateo.marchisone@gmail.com>
2021-11-15 22:28:50 -0300

    designed Libros and Revistas windows

---
+++ vLibros.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class vLibros {
+    private JPanel panel1;
+    private JComboBox comboBoxLibros;
+    private JLabel lbTitulo;
+    private JLabel lbAutor;
+    private JLabel lbCategoria;
+    private JLabel lbEditorial;
+    private JButton cerrarButton;
+}
---
+++ vRevistas.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class vRevistas {
+    private JComboBox comboBoxRevista;
+    private JLabel lbTitulo;
+    private JLabel lbAutor;
+    private JLabel lbCategoria;
+    private JLabel lbEditorial;
+    private JButton cerrarButton;
+}
